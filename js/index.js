43ab1c6ef6d8b45f20380031665c688536edaf04
mariadudasn <maria.santos081207@gmail.com>
2023-09-26 10:48:03 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,42 +1,6 @@
-// export default function faq{
+import faq from "./modules/faq.js"
 
-    const imagens = document.querySelectorAll('.js-imagens li')
-    const textos = document.querySelectorAll('.js-conteudo section')
-    const perguntah3 = document.querySelectorAll('.js-pergunta h3')
-    const resposta = document.querySelectorAll('.js-pergunta p')
-    const seta = document.querySelectorAll('.js-pergunta span')
-
-
-    function ativarConteudo(indice){
-        textos.forEach((item) => {
-            item.classList.remove('active')
-        })
-        textos[indice].classList.add('active')
-    }
-
-    imagens.forEach((item,indice) => {
-    item.addEventListener('click', () =>{
-            ativarConteudo(indice)
-    })
-    })
-
-    function ativardesativarConteudo(indice){
-        resposta[indice].classList.toggle('active')
-        if (resposta[indice].classList.contains('active') == true) {
-            seta[indice].innerText = "▴"
-        }else{
-            seta[indice].innerText = "▾";
-        }
-    }
-    
-    perguntah3.forEach((item,indice) => {
-        item.addEventListener('click', () =>{
-            ativardesativarConteudo(indice)
-    })
-    })
-
-
-
+faq()
 
 
 
