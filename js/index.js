6624c40d03d6e3a68da1c6922ca3bf4559e1c4a4
mariadudasn <maria.santos081207@gmail.com>
2023-09-26 10:29:40 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,32 +1,42 @@
-const imagens = document.querySelectorAll('.js-imagens li')
-const textos = document.querySelectorAll('.js-conteudo section')
-const perguntah3 = document.querySelectorAll('.js-pergunta h3')
-const resposta = document.querySelectorAll('.js-pergunta p')
-const setinhaa = document.querySelectorAll('.js-pergunta span')
+// export default function faq{
+
+    const imagens = document.querySelectorAll('.js-imagens li')
+    const textos = document.querySelectorAll('.js-conteudo section')
+    const perguntah3 = document.querySelectorAll('.js-pergunta h3')
+    const resposta = document.querySelectorAll('.js-pergunta p')
+    const seta = document.querySelectorAll('.js-pergunta span')
 
 
-function ativarConteudo(indice){
-    textos.forEach((item) => {
-        item.classList.remove('active')
-    })
-    textos[indice].classList.add('active')
-}
+    function ativarConteudo(indice){
+        textos.forEach((item) => {
+            item.classList.remove('active')
+        })
+        textos[indice].classList.add('active')
+    }
 
-imagens.forEach((item,indice) => {
-   item.addEventListener('click', () =>{
-        ativarConteudo(indice)
-   })
-})
-
-function ativardesativarConteudo(indice){
-    resposta[indice].classList.toggle('active')
-}
-
-perguntah3.forEach((item,indice) => {
+    imagens.forEach((item,indice) => {
     item.addEventListener('click', () =>{
-        ativardesativarConteudo(indice)
-})
-})
+            ativarConteudo(indice)
+    })
+    })
+
+    function ativardesativarConteudo(indice){
+        resposta[indice].classList.toggle('active')
+        if (resposta[indice].classList.contains('active') == true) {
+            seta[indice].innerText = "▴"
+        }else{
+            seta[indice].innerText = "▾";
+        }
+    }
+    
+    perguntah3.forEach((item,indice) => {
+        item.addEventListener('click', () =>{
+            ativardesativarConteudo(indice)
+    })
+    })
+
+
+
 
 
 
