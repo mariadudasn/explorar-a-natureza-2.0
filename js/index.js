b8aee179732fd9273de7e23e4533486b352d7493
mariadudasn <maria.santos081207@gmail.com>
2023-10-03 10:33:08 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import faq from "./modules/faq.js"
+import menu from "./modules/menu.js"
 
 faq()
 menu()
-
 
--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,3 +1,17 @@
 export default function menu(){
+
+    const toggle = document.querySelector('.toggle');
+    const menuList = document.querySelector('.js-menu');
     
+    if (toggle && menuList) {
+        function toggleMenu() {
+            menuList.classList.toggle('active');
+            const toggleIcon = toggle.querySelector('span');
+            toggleIcon.classList.toggle('active');
+            toggleIcon.innerText = toggleIcon.innerText === 'menu' ? 'close' : 'menu';
+        }
+    
+        toggle.addEventListener('click', toggleMenu);
+    }
 }
+    
