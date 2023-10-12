1e60062ec0da8a7e41f737079a725005e14f7abc
mariadudasn <maria.santos081207@gmail.com>
2023-10-12 18:35:24 -0300

    Update

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,17 +1,30 @@
 export default function modal(){
-    const form = document.querySelector('.js-modal')
+    const modal = document.querySelector('.js-modal')
+    const forms = document.querySelector(".modal-forms")
+    const close = document.querySelector(".modal-close")
     const dados = {}
+
+    if (localStorage.getItem("email") === null) {
+        modal.classList.add("active");
+      }
+    else {
+        modal.classList.remove("active");
+    }
 
     function pegarValorForm(event){
         dados[event.target.name] = event.target.value
         console.log(dados)
     }
 
-    form.addEventListener('change', pegarValorForm)
-    form.addEventListener('submit', (event) => {
+    modal.addEventListener('change', pegarValorForm)
+    forms.addEventListener('submit', (event) => {
         event.preventDefault()
         localStorage.setItem(dados.email, JSON.stringify(dados))
         alert("Dados salvos com sucesso")
-    })
+        modal.classList.remove("active");
+    });
 
+    close.addEventListener("click", () => {
+        modal.classList.remove("active");
+    });
 }
