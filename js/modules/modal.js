32a70518be250f7575b9287072e27117d654f000
mariadudasn <maria.santos081207@gmail.com>
2023-10-13 23:51:34 -0300

    Update

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,48 +1,58 @@
-export default function modal(){
-    const modal = document.querySelector('.js-modal')
-    const forms = document.querySelector(".modal-forms")
-    const close = document.querySelector(".modal-close")
-    const dados = {}
+export default function modal() {
+    const modal = document.querySelector('.js-modal');
+    const forms = document.querySelector('.modal-forms');
+    const close = document.querySelector('.modal-close');
+    const emailInput = document.querySelector('input[name="email"]');
+    const blur = document.querySelector('.blur');
 
-    
+    const dados = {};
     let timeout;
 
     function mostrarModal() {
         modal.classList.add("active");
+        blur.classList.add("active");
         timeout = setTimeout(() => {
-            if (localStorage.getItem("email") === null) {
-                modal.classList.add("active");
-            }
-            else {
-                clearInterval(interval);
-                modal.classList.remove("active");
+            if (!dados.email) {
+                esconderModal();
+                mostrarModal();
             }
         }, 5000);
     }
 
-    function pegarValorForm(event){
-        dados[event.target.name] = event.target.value
-        console.log(dados)
-        if (event.target.name === "email") {
+    function esconderModal() {
+        modal.classList.remove("active");
+        blur.classList.remove("active");
+    }
+
+    if (localStorage.getItem("email") === null) {
+        mostrarModal();
+    }
+
+    function pegarValorForm(event) {
+        dados[event.target.name] = event.target.value;
+        console.log(dados);
+        if (event.target.name === "email" && event.target.value !== "") {
+            esconderModal();
             clearTimeout(timeout);
         }
     }
 
-    modal.addEventListener('change', pegarValorForm)
-    forms.addEventListener('submit', (event) => {
-        event.preventDefault()
-        localStorage.setItem('email', dados.email); 
-        JSON.stringify(dados);
-        alert("Dados salvos com sucesso")
-        modal.classList.remove("active");
-        clearTimeout(timeout); 
+    forms.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (dados.email && dados.email !== "") {
+            localStorage.setItem("email", JSON.stringify(dados.email));
+            alert("Dados salvos com sucesso");
+            esconderModal();
+            clearTimeout(timeout);
+        } else {
+            alert("Por favor, insira seu e-mail.");
+        }
     });
 
     close.addEventListener("click", () => {
-        modal.classList.remove("active");
-        clearTimeout(timeout);
-        mostrarModal();
+        esconderModal();
+        setTimeout(mostrarModal, 5000);
     });
 
-    mostrarModal();
+    emailInput.addEventListener('input', pegarValorForm);
 }
